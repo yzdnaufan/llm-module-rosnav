3a1e90a81c6e2385c06f50e769a81a8e356225dc
yzdnaufan <ahyaznau@gmail.com>
2025-02-03 21:40:11 +0700

    add error handling

--- routes/agent.py
+++ routes/agent.py
@@ -11,5 +11,9 @@ def rag(request: AgentRequest):
     try:
         result = graph.invoke_graph(request.text)
         return result
+    except ValueError as ve:
+        raise HTTPException(status_code=400, detail=str(ve))
+    except TypeError as te:
+        raise HTTPException(status_code=422, detail=str(te))
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
